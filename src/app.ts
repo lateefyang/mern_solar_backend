ac9658aaeb9ce132c65129661c2cdd4f3c2089bd
lateefyang <75533739+lateefyang@users.noreply.github.com>
2025-03-25 17:14:31 -0700

    Cleanup imports, exports and style

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
-import {Request, Response, NextFunction } from 'express'
+import createError from 'http-errors'
+import express, { Request, Response, NextFunction } from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-
-var app = express()
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -40,4 +38,4 @@ app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
   res.render('error')
 })
 
-module.exports = app
+export default app
